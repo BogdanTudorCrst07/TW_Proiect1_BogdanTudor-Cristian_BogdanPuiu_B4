233e92837984e8fe12462fb7ca3c7cd93c6c60ac
Bogdan Puiu <bogdanpuiu70@gmail.com>
2021-06-20 22:34:28 +0300

    fixed minor login bug

--- TW_Proiect1_BogdanTudor-Cristian_BogdanPuiu_B4-main/Proiect TW/Proiect TW/Login/login.js
+++ TW_Proiect1_BogdanTudor-Cristian_BogdanPuiu_B4-main/Proiect TW/Proiect TW/Login/login.js
@@ -1,6 +1,6 @@
 
-function login(){
-    var user={
+function login() {
+    var user = {
         name: document.getElementsByName("name")[0].value,
         password: document.getElementsByName("password")[0].value
     }
@@ -9,31 +9,36 @@ function login(){
         {
             method: 'POST',
             headers: {
-                'Content-Type':'application/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(user)
         }
     ).then(res => res.json()).then(function (response) {
-           window.localStorage.setItem("auth",response)
-           document.getElementById("homeBtn").click()
-           alert("You are now logged in!")
-        })
+        if (response.success == false) {
+                    alert("Connection failed. Invalid username/password")
+        }
+       else {window.localStorage.setItem("auth", response)
+        document.getElementById("homeBtn").click()
+        alert("You are now logged in!")
+    }
+
+    })
 }
-function logout(){
-    var aux=window.localStorage.getItem("auth")
-    window.localStorage.removeItem("auth",aux)
+function logout() {
+    var aux = window.localStorage.getItem("auth")
+    window.localStorage.removeItem("auth", aux)
 }
 
-if(window.localStorage.getItem("auth")!=null){
+if (window.localStorage.getItem("auth") != null) {
     document.addEventListener('DOMContentLoaded', (event) => {
-       document.getElementById("loginBtn").style.display="none";
-       document.getElementById("logoutBtn").style.display="visible";
-      })
+        document.getElementById("loginBtn").style.display = "none";
+        document.getElementById("logoutBtn").style.display = "visible";
+    })
 }
-else{
+else {
     document.addEventListener('DOMContentLoaded', (event) => {
-        document.getElementById("logoutBtn").style.display="none";
-       })
+        document.getElementById("logoutBtn").style.display = "none";
+    })
 }
 
 function favorites() {
